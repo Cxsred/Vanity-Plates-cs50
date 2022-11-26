be1e17cccc2471a1dfeab14a314a1f5f5596b888
Cbk <cbkorucu@gmail.com>
2022-11-26 19:09:24 +0300

    Create plates.py

---
+++ plates.py
@@ -0,0 +1,31 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(b):
+    if 6 >= len(b) >= 2 and b [0:2].isalpha() and b.isalnum():
+        for char in b:
+            if char.isdigit():
+                index = b.index(char)
+                if b[index:].isdigit()and int(char) !=0:
+
+                        return True
+                else:
+                    return False
+        return True
+
+
+
+
+
+
+
+
+
+
+
+main()
